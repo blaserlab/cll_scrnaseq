cd3696dcb365eb483c845ef260de39906d7814cc
blaserlab <bradley.blaser@osumc.edu>
2021-05-28 10:03:46 -0400

    reprocessed with MRD samples; restructure repo to move out data
    Former-commit-id: 2cc5524b70429559475ec341aacdea151dc2e1eb

---
+++ R/dependencies.R
@@ -0,0 +1,15 @@
+# libraries-------------------------------------
+library(blaseRtools)# devtools::install_github("git@github.com:blaserlab/blaseRtools.git")
+library(tidyverse)
+library(monocle3)
+library(circlize)
+library(ComplexHeatmap)
+library(lazyData)
+library(cowplot)
+library(RColorBrewer)
+library(fastSave)
+library(ggrepel)
+
+# project data-------------------------------------
+# run once to load, run again to unload
+# requireData(package = "",quietly = F)
