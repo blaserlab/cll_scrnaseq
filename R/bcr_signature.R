77238fc9a8ef3fd64d6b6cd922818df816316300
blaserlab <bradley.blaser@osumc.edu>
2022-04-23 07:04:01 -0400

    made bcr gene heatmap
    Former-commit-id: 11eaade6e14f9bd814677c2c78b02e918484e3b8

--- R/bcr_signature.R
+++ R/bcr_signature.R
@@ -4,8 +4,6 @@ nejm_bcr_sig_genes <- nejm_bcr_sig$gene_short_name
 
 
 rowData(cds_main)$nejm_bcr_sig <- ifelse(rowData(cds_main)$gene_short_name %in% nejm_bcr_sig_genes, "yes_bcr", "no_bcr")
-bb_rowmeta(cds_main) |> View()
-bb_cellmeta(cds_main) |> glimpse()
 cds_main_bcr_only <- cds_main[rowData(cds_main)$nejm_bcr_sig == "yes_bcr",
                               colData(cds_main)$partition_assignment == "B"]
 
@@ -16,3 +14,20 @@ nejm_bcr_umap_faceted <- nejm_bcr_umap + facet_grid(cols = vars(timepoint_merged
 save_plot(nejm_bcr_umap, filename = file.path(network_out, "nejm_bcr_umap.png"), base_height = 4, base_width = 5)
 
 save_plot(nejm_bcr_umap_faceted, filename = file.path(network_out, "nejm_bcr_umap_faceted.png"), base_height = 10, base_width = 6.5)
+
+
+bb_cellmeta(cds_main_bcr_only)
+
+colData(cds_main_bcr_only)$type_timepoint <- paste0(colData(cds_main_bcr_only)$patient_type, "_", colData(cds_main_bcr_only)$timepoint)
+colData(cds_main_bcr_only)$type_timepoint <- factor(colData(cds_main_bcr_only)$type_timepoint, 
+                                                    levels = c("BTK_baseline", "BTK_btk_clone", "BTK_relapse", "MRD_baseline", "MRD_3yrs", "MRD_5yrs"))
+
+nejm_mtx <- aggregate_gene_expression(cds_main_bcr_only, cell_group_df = bb_cellmeta(cds_main_bcr_only) |> select(cell_id, type_timepoint), scale_agg_values = F) |> 
+  as.matrix() |> 
+  t() |> 
+  scale() |> 
+  t()
+rownames(nejm_mtx) <- left_join(tibble(feature_id = rownames(nejm_mtx)), bb_rowmeta(cds_main_bcr_only)) |> pull(gene_short_name)
+
+
+Heatmap(nejm_mtx, cluster_columns = F)
