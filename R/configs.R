1846962656cd094f609adcb5bd4ea6160020b520
blaserlab <bradley.blaser@osumc.edu>
2024-09-05 11:12:09 -0400

    moved some object mods to data package; added clinical flow data

--- R/configs.R
+++ R/configs.R
@@ -74,58 +74,14 @@ timepoint_palette <- c("1" = brewer.pal(3, "YlGn")[1],
                        "2" = brewer.pal(3, "YlGn")[2],
                        "3" = brewer.pal(3, "YlGn")[3])
 
-tcell_genes <- read_csv("~/network/X/Labs/Blaser/share/collaborators/cll_scrnaseq_manuscript/queries/tcell_genes.csv")
-
-NK_genes <- read_csv("~/network/X/Labs/Blaser/share/collaborators/cll_scrnaseq_manuscript/queries/NK_genes.csv") 
 
 tcell_genes_palette <- tcell_genes |> group_by(label) |> summarise() |> mutate(col = brewer.pal(7, "Accent")) |> deframe()
 
 nk_genes_palette <- NK_genes |> group_by(label) |> summarise() |> mutate(col = brewer.pal(3, "Accent")[1:2]) |> deframe()
 
-# cds_mods
-colData(cds_main)$timepoint_merged_1 <- recode(colData(cds_main)$timepoint_merged, "baseline" = "1",
-                                               "3yrs|btk_clone" = "2",
-                                               "5yrs|relapse" = "3")
-colData(cds_main)$timepoint_merged_2 <- recode(colData(cds_main)$timepoint_merged, "baseline" = "Timepoint 1",
-                                               "3yrs|btk_clone" = "Timepoint 2",
-                                               "5yrs|relapse" = "Timepoint 3")
-
-colData(cds_main)$patient_type1 <- recode(colData(cds_main)$patient_type, "BTK" = "resistant", "MRD" = "responsive")
-colData(cds_main)$patient_type2 <- recode(colData(cds_main)$patient_type, "BTK" = "resistant", "MRD" = "sensitive")
-
-leiden_l1_assignment <- bb_cellmeta(cds_main) |> 
-  count(leiden, seurat_celltype_l1) |> 
-  group_by(leiden) |> 
-  slice_max(order_by = n, n = 1) |> 
-  select(leiden, seurat_celltype_l1) |> 
-  deframe()
-
-colData(cds_main)$leiden_l1_assignment <- recode(colData(cds_main)$leiden, !!!leiden_l1_assignment)
-
-seurat_l2_leiden_consensus <- bb_cellmeta(cds_main) |> 
-  group_by(leiden, seurat_celltype_l2) |> 
-  summarise(n = n()) |> 
-  slice_max(order_by = n, n = 1) |> 
-  select(leiden, seurat_celltype_l2) |> 
-  deframe()
-
-colData(cds_main)$seurat_l2_leiden_consensus <- recode(colData(cds_main)$leiden, !!!seurat_l2_leiden_consensus)
-colData(cds_main)$seurat_l2_leiden_consensus <- as.character(colData(cds_main)$seurat_l2_leiden_consensus)
-
-colData(cds_main)$leiden_comparison_renamed <- recode(colData(cds_main)$leiden_comparison, 
-                                                      "20" = "Stressed",
-                                                      "24" = "Inflammatory 1",
-                                                      "34" = "Inflammatory 2",
-                                                      "other" = "Ig-producing")
-
 network_out <- fs::path("~/network/X/Labs/Blaser/share/collaborators/cll_scrnaseq_manuscript/Paper/r_out/")
 network_tables <- fs::path("~/network/X/Labs/Blaser/share/collaborators/cll_scrnaseq_manuscript/tables")
 paper_tables <- fs::path("~/network/X/Labs/Blaser/share/collaborators/cll_scrnaseq_manuscript/Paper/tables")
-
-# source local configs ----------------------------------------------------
-# these are sourced after main configs and will overwrite duplicate entries if
-# present. The file local_configs.R is ignored by git and so is useful for user-
-# specific configurations such as output directories or formatting.
 
 
 
